bce01a99af69425930d3e45176701ea3c5c84f4e
Daniel Alexander <danielalexander@Daniels-Mac-mini.local>
2023-10-30 01:33:36 +1100

    finalizing datamart 1

---
+++ bde_assignment_3/models/datamart/dm_listing_neighbourhood.sql
@@ -0,0 +1,101 @@
+
+with source as (
+    select *,
+    EXTRACT(MONTH FROM scraped_date) AS extracted_month,
+    EXTRACT(YEAR FROM scraped_date) AS extracted_year
+   
+    from warehouse.fact_listings
+),
+
+active_listing as (
+
+select 
+listing_neighbourhood,
+extracted_year,
+extracted_month,
+COUNT (*) as active_listing,
+MIN(price) AS min_price_active_listing,
+MAX(price) AS max_price_active_listing,
+PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY price) AS median_price_active_listing,
+AVG(price) AS avg_price_active_listing,
+COALESCE(AVG(CASE WHEN review_scores_rating = 'NaN' THEN 0 ELSE review_scores_rating END), 0) AS avg_review_scores_rating,
+AVG (price * (30 - availability_30) ) as average_estimated_revenue_active_listing
+
+FROM source
+where source.has_availability='t'
+GROUP BY listing_neighbourhood,extracted_year,extracted_month
+
+),
+
+
+inactive_listing as (
+
+select 
+listing_neighbourhood,
+extracted_month,
+extracted_year,
+COUNT (*) as inactive_listing
+
+FROM source
+where source.has_availability='f'
+GROUP BY listing_neighbourhood,extracted_year,extracted_month
+
+
+),
+
+
+all_listing as (
+
+select 
+listing_neighbourhood,
+extracted_month,
+extracted_year,
+COUNT (*) as total_listing,
+count(DISTINCT host_id) as number_distinct_host,
+SUM(CASE WHEN host_is_superhost = 't' THEN 1 ELSE 0 END) AS superhost_count,
+SUM(30 - availability_30) AS total_number_of_stays
+
+
+FROM source
+GROUP BY listing_neighbourhood,extracted_year,extracted_month
+
+)
+SELECT 
+
+all_listing.listing_neighbourhood,
+all_listing.extracted_month as month,
+all_listing.extracted_year as year,
+active_listing.active_listing*100/all_listing.total_listing as active_listing_rate,
+active_listing.min_price_active_listing,
+active_listing.max_price_active_listing,
+active_listing.median_price_active_listing,
+active_listing.avg_price_active_listing,
+all_listing.number_distinct_host,
+all_listing.superhost_count*100/all_listing.number_distinct_host as superhost_rate,
+active_listing.avg_review_scores_rating,
+(active_listing.active_listing - LAG(active_listing.active_listing, 1) OVER (partition by all_listing.listing_neighbourhood ORDER BY all_listing.extracted_year,all_listing.extracted_month))*100/LAG(active_listing.active_listing, 1) OVER (partition by all_listing.listing_neighbourhood ORDER BY all_listing.extracted_year,all_listing.extracted_month) as percentage_change_active_listing, 
+(inactive_listing.inactive_listing - LAG(inactive_listing.inactive_listing, 1) OVER (partition by inactive_listing.listing_neighbourhood ORDER BY inactive_listing.extracted_year,all_listing.extracted_month))*100/LAG(inactive_listing.inactive_listing, 1) OVER (partition by all_listing.listing_neighbourhood ORDER BY all_listing.extracted_year,all_listing.extracted_month) as percentage_change_inactive_listing,
+
+
+all_listing.total_number_of_stays,
+active_listing.average_estimated_revenue_active_listing
+
+
+
+
+FROM
+all_listing 
+left join active_listing on all_listing.listing_neighbourhood  = active_listing.listing_neighbourhood
+    AND all_listing.extracted_month = active_listing.extracted_month
+    AND all_listing.extracted_year = active_listing.extracted_year
+left join inactive_listing on all_listing.listing_neighbourhood  = inactive_listing.listing_neighbourhood
+    AND all_listing.extracted_month = inactive_listing.extracted_month
+    AND all_listing.extracted_year = inactive_listing.extracted_year
+    
+
+
+      
+
+    
+    
+ 
